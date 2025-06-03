365c9fcc978b25439b182a01c7d64ce982086cb7
takxz <164672018+takxz@users.noreply.github.com>
2025-06-03 11:07:15 +0200

    second commit

---
+++ app/src/main/java/com/example/partie2_corentin_leys/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.partie2_corentin_leys
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.partie2_corentin_leys.ui.theme.PARTIE2CORENTINLEYSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PARTIE2CORENTINLEYSTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PARTIE2CORENTINLEYSTheme {
+        Greeting("Android")
+    }
+}
